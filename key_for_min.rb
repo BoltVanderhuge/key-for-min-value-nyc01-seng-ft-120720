99ee372cd15f38e1d2deb9cd97e26928c100c287
John Wisneski <john.j.wisneski@gmail.com>
2020-11-30 00:17:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,11 @@ def key_for_min_value(name_hash)
   #   nil
   # end
   
+<<<<<<< HEAD
   min_key = nil
+=======
+  min_key = ""
+>>>>>>> 41004bf9206f4acca7ee98b5a8c0f4b08baf84fd
   min_value = nil
   name_hash.each do |key, value|
     if min_value == nil || value <= min_value 
